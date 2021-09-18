e7575269754101fc8dfb888240861087703bf821
Said-Aabilla <said160aabilla@gmail.com>
2021-09-18 14:55:52 +0000

    used morgan middlware with express

--- node-express/index.js
+++ node-express/index.js
@@ -3,15 +3,17 @@ const express = require('express'),
 
 const hostname = 'localhost';
 const port = 3000;
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('dev'));
+app.use(express.static(__dirname + '/public'));
+
 app.use((req, res, next) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
-
 });
 
 const server = http.createServer(app);
