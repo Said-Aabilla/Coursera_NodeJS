5b5d3e352f951c636498bd22b058e8412cd93269
Said-Aabilla <said160aabilla@gmail.com>
2021-09-19 11:54:11 +0000

    Routes fix in Express simple http REST

--- node-express/index.js
+++ node-express/index.js
@@ -12,48 +12,16 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
+const dishRouter = require('./routes/dishRouter');
+app.use('/dishes', dishRouter);
 
-app.all('/dishes', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    next();
-});
+const promotionRouter = require('./routes/promotionRouter');
+app.use('/promotions', promotionRouter);
 
-app.get('/dishes', (req, res, next) => {
-    res.end('Will send all the dishes to you!');
-});
+const leaderRouter = require('./routes/leaderRouter');
+app.use('/leaders', leaderRouter);
 
-app.post('/dishes', (req, res, next) => {
-    res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
-});
 
-app.put('/dishes', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('PUT operation not supported on /dishes');
-});
-
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all dishes');
-});
-
-app.get('/dishes/:dishId', (req, res, next) => {
-    res.end('Will send details of the dish: ' + req.params.dishId + ' to you!');
-});
-
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/' + req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name +
-        ' with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dish: ' + req.params.dishId);
-});
 
 app.use(express.static(__dirname + '/public'));
 
