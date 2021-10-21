ba7f0d7e7ad124bd9ed2eaa5ae96f9113c7516d1
Said-Aabilla <said160aabilla@gmail.com>
2021-10-21 12:06:25 +0000

    Node and Mongoose

---
+++ node-mongoose/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const Dishes = require('./models/dishes');
+
+const url = 'mongodb://localhost:27017/restorante';
+const connect = mongoose.connect(url);
+
+connect.then((db) => {
+
+    console.log('Connected correctly to server');
+
+    Dishes.create({
+        name: 'Uthappizza',
+        description: 'test'
+    })
+        .then((dish) => {
+            console.log(dish);
+
+            return Dishes.find({});
+        })
+        .then((dishes) => {
+            console.log(dishes);
+
+            return Dishes.deleteOne({});
+        })
+        .then(() => {
+            return mongoose.connection.close();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+
+});
